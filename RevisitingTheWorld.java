15486c6d36c701f01d77f14476021169505dec7a
youssef <44912677hd-youssef@users.noreply.github.com>
2018-11-27 12:04:32 -0500

    More edits to revisiting the world
    added scanner to revisiting the world

--- RevisitingTheWorld.java
+++ RevisitingTheWorld.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
 public class RevisitingTheWorld{
    public static void main(String[]args){
-   
-   
-   System.out.println("Hello World");
+
+
+
+
+	   Scanner casere = new Scanner(System.in);
+
+	   System.out.println("Hello World");
+
+	   System.out.println("Which world do you want to go to? ");
+	   String airbnb = casere.nextLine();
+
    }
 }
